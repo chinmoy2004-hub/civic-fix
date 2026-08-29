546364714ba04451bbb27fd5eabbac6545de73b7
Chinmoy-19 <chinmoygoswami744@gmail.com>
2026-08-29 12:03:45 +0530

    Fix Render allowed host

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   preview: {
+    allowedHosts: ['civic-fix-1-h858.onrender.com'],
+  },
 })
